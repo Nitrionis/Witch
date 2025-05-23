f86a1f40971d5f31a7270ed4f4de92b203491aa7
Dmitry Shcherbina <nitrodimon@mail.ru>
2025-05-23 23:04:05 +0300

    Use hash to check if Heights changed

--- Assets/Game/Systems/Components/Chunk.cs
+++ Assets/Game/Systems/Components/Chunk.cs
@@ -1,7 +1,13 @@
-﻿
+﻿using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
 namespace Assets.Game.Systems.Components
 {
 	internal struct Chunk
 	{
+		public double2 Position;
+		public NativeArray<ushort> Heights;
+		public Hash128 HeightsHash;
 	}
 }
