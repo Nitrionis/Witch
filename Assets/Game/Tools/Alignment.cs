dbe0bd9aedee45d957ed33ace170a747478d33f0
Dmitry Shcherbina <nitrodimon@mail.ru>
2025-05-30 20:12:00 +0300

    Add Alignment

---
+++ Assets/Game/Tools/Alignment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Assets.Game.Tools
+{
+	internal class Alignment<T> where T : unmanaged
+	{
+		private static int value;
+
+		public static int Get()
+		{
+			if (value != 0) {
+				return value;
+			}
+			value = CalculateAlignment(typeof(T));
+			return value;
+		}
+
+		public static int CalculateAlignment(Type type)
+		{
+			int alignment = 1;
+			var fields = type.GetFields();
+
+			foreach (var field in fields) {
+				int fieldAlignment = 1;
+
+				if (field.FieldType == typeof(byte) || field.FieldType == typeof(sbyte))
+					fieldAlignment = 1;
+				else if (field.FieldType == typeof(short) || field.FieldType == typeof(ushort))
+					fieldAlignment = 2;
+				else if (field.FieldType == typeof(int) || field.FieldType == typeof(uint) ||
+						 field.FieldType == typeof(float))
+					fieldAlignment = 4;
+				else if (field.FieldType == typeof(long) || field.FieldType == typeof(ulong) ||
+						 field.FieldType == typeof(double))
+					fieldAlignment = 8;
+				else if (field.FieldType == typeof(IntPtr) || field.FieldType == typeof(UIntPtr))
+					fieldAlignment = IntPtr.Size;
+				else if (field.FieldType.IsValueType) // For nested structs
+					fieldAlignment = CalculateAlignment(field.FieldType);
+
+				if (fieldAlignment > alignment)
+					alignment = fieldAlignment;
+			}
+
+			return alignment;
+		}
+	}
+}
